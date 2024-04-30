ab8eac72bed821dfed976a96821d054cf36b557a
Jawed <148317935+Wedja10@users.noreply.github.com>
2024-04-30 16:55:45 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const icons = document.getElementById("icons");
+    const nav = document.getElementById("nav");
+
+    icons.addEventListener("click", () => {
+        nav.classList.toggle("active");
+    });
+});
